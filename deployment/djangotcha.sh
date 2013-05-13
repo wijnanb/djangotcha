cdd483940cc6ba89c2099a85927b363cc6c240b6
Maarten Huijsmans <maarten.huijsmans@gmail.com>
2013-05-13 19:51:20 +0200

    another port

--- deployment/djangotcha.sh
+++ deployment/djangotcha.sh
@@ -3,5 +3,5 @@ ROOT="/home/ubuntu/git/djangotcha"
 
 cd $ROOT
 . /home/ubuntu/.virtualenvs/djangotcha/bin/activate
-exec /home/ubuntu/.virtualenvs/djangotcha/bin/python manage.py run_gunicorn -b 0.0.0.0:8000
+exec /home/ubuntu/.virtualenvs/djangotcha/bin/python manage.py run_gunicorn -b 0.0.0.0:8001
 
