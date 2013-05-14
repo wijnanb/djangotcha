6353bcd012c471854fe930efb1e209bf2ee31d27
lukin0110 <maarten.huijsmans@gmail.com>
2013-05-14 02:03:43 +0200

    reconfig for nginx and gunicorn

--- deployment/djangotcha.sh
+++ deployment/djangotcha.sh
@@ -4,6 +4,6 @@ ROOT="/home/ubuntu/git/djangotcha"
 cd $ROOT
 . /home/ubuntu/.virtualenvs/djangotcha/bin/activate
 exec /home/ubuntu/.virtualenvs/djangotcha/bin/python manage.py run_gunicorn -b 0.0.0.0:8001 \
-    -log-file=/var/log/djangotcha.log
+    --log-file=/var/log/djangotcha.log
 
 
