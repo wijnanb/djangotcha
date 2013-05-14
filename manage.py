87e94a3ab92271cf8f36a603a83e3a6f1bce937b
Bert Wijnants <bert.wijnants@gmail.com>
2013-05-14 11:44:16 +0200

    break the server

--- manage.py
+++ manage.py
@@ -3,11 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    try:
-        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangotcha.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djangotcha.settings")
 
-        from django.core.management import execute_from_command_line
-        execute_from_command_line(sys.argv)
-    except:
-        import traceback
-        print traceback.format_exc()
+    from django.core.management import execute_from_command_line
+    execute_from_command_line(sys.argv)
